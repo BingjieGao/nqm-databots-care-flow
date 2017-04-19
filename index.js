181cd5bbdc78ee9d03ee9d7b60f549d3eb40af6d
Bingjie Gao <gilman.david04@gmail.com>
2017-04-19 15:18:34 +0100

    Added comments, removed databot-test thing

--- index.js
+++ index.js
@@ -2,21 +2,27 @@ module.exports = (function() {
   "use strict";
   const fs = require("fs");
   function databot(input, output, context) {
-    // just for local test if generated json file is corret
-    output.setFileStorePath("./jsonFiles");
-    // Generate the output file based on the given output name input.
-    const outputFilePath = output.getFileStorePath(`${context.instanceId}-output.json`);
-    // const outputFilePath = output.generateFileStorePath("json");
+    const outputFilePath = output.generateFileStorePath("json");
      // Create the output stream.
     const destStream = fs.createWriteStream(outputFilePath, {flags: input.appendOutput ? "a" : "w"});
+
     // decide to fire areaService poplet databot or areaService demand databot
     let databotType;
     if (input.databotType === "poplet") {
       databotType = require("./lib/behaviour-function-factory");
     } else if (input.databotType === "demand") {
       databotType = require("./lib/demand-databot");
+    } else {
+      output.debug("NOT valid databotType input");
     }
-    databotType(input, context).then((databot) => {
+
+    // the UUID of areaServiceDataset
+    // refers to databot inputs dataset ids
+    const areaServiceId = input.areaServiceId;
+    const areaServiceDataset = context.packageParams.areaServiceDataset;
+
+    databotType(areaServiceId, areaServiceDataset, context.tdxApi)
+    .then((databot) => {
       return databot(input, output, context, destStream);
     })
     .then(() => {
@@ -28,17 +34,7 @@ module.exports = (function() {
       output.debug(err.message);
     });
   }
-
-  let input = null;
-  if (process.env.NODE_ENV === "test") {
-    // Requires nqm-databot-gpsgrab.json file for testing
-    input = require("./databot-test.js")(process.argv[2]);
-  } else {
-    // Load the nqm input module for receiving input from the process host.
-    input = require("nqm-databot-utils").input;
-  }
-
-// Read any data passed from the process host. Specify we're expecting JSON data.
+  const input = require("nqm-databot-utils").input;
   input.pipe(databot);
 }());
 
